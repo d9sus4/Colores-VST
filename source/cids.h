461793197ad5ca494b31c0f7056cdb3a2d25803b
Delphynium <dlphynm@gmail.com>
2023-12-26 04:50:08 -0800

    init project

---
+++ source/cids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2023 Dsus.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace dsus {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kColoresProcessorUID (0xD3A344B4, 0x009559AF, 0xBB017F98, 0x38072431);
+static const Steinberg::FUID kColoresControllerUID (0x57072BAC, 0x6788520E, 0x8A20F5B9, 0xAE8E1BA5);
+
+#define ColoresVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace dsus
